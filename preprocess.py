e531ea77d677e5108218d885238be5f1ada23800
Chenqin Yang <cy1355@nyu.edu>
2020-04-25 18:11:32 -0400

    add polar images

--- preprocess.py
+++ preprocess.py
@@ -76,7 +76,10 @@ class LabeledDataset(torch.utils.data.Dataset):
             image = Image.open(image_path)
             images.append(self.transform(image))
         image_tensor = torch.stack(images)
-        
+
+        ######################################################
+        image_tensor_cat = torch.cat(images, dim = 2)
+        ######################################################
 
         data_entries = self.annotation_dataframe[(self.annotation_dataframe['scene'] == scene_id) & (self.annotation_dataframe['sample'] == sample_id)]
         corners = data_entries[['fl_x', 'fr_x', 'bl_x', 'br_x', 'fl_y', 'fr_y','bl_y', 'br_y']].to_numpy()
@@ -103,7 +106,7 @@ class LabeledDataset(torch.utils.data.Dataset):
 
             return image_tensor, target, road_image, extra
         else:
-            return image_tensor, target, road_image, sample_path
+            return image_tensor, target, road_image, sample_path, image_tensor_cat
 
 transform = torchvision.transforms.ToTensor()
 labeled_trainset = LabeledDataset(image_folder=image_folder,
@@ -120,25 +123,82 @@ modules = list(pretrained_model.children())[:-3]
 res_model = nn.Sequential(*modules)
 res_model.eval()
 
-cnt = 0
-for data, _, road_img, filename in trainloader:
 
-    output = res_model(data[0])
+##########################################################
+# save camera-level tensor
+# output shape: [6, 256, 16, 20]
+##########################################################
+# cnt = 0
+# for data, _, road_img, filename in trainloader:
+
+#     output = res_model(data[0])
+#     save_filename = filename[0].split('/')[-2] + '_' + filename[0].split('/')[-1]
+    
+#     if not os.path.exists('/Users/leo/Downloads/result/image_tensor'):
+#         os.mkdir('/Users/leo/Downloads/result/image_tensor')
+#     if not os.path.exists('/Users/leo/Downloads/result/road_map'):
+#         os.mkdir('/Users/leo/Downloads/result/road_map')
+
+#     assert road_img[0].shape == torch.Size([800, 800])
+#     assert output.shape == torch.Size([6, 256, 16, 20])
+
+#     np.save(os.path.join('/Users/leo/Downloads/result/road_map', save_filename), road_img[0].detach().numpy())    
+#     np.save(os.path.join('/Users/leo/Downloads/result/image_tensor', save_filename), output.detach().numpy())
+    
+#     cnt += 1
+#     if cnt % 100 == 0:
+#         print(str(28 * 126 - cnt) + ' left')
+
+
+##########################################################
+# save polar image
+##########################################################
+
+def warpper(data):
+    
+    img = data[0].permute(1,2,0).numpy()
+    img = (img * 255).astype(np.uint8)
+    
+    img = np.rot90(img)
+    img = np.flip(img, 1)
+    # (width, height * 1.5, 3)
+    new_img = np.zeros((1836, 390, 3))
+    # -height
+    new_img[:, -256:, :] = img
+    img = new_img
+    img = cv2.resize(img, (3000, 3000))
+    value = np.sqrt(((img.shape[0]/2.0)**2.0)+((img.shape[1]/2.0)**2.0))
+    polar_image = cv2.linearPolar(src=img, center=(1500, 1500), maxRadius=1500, flags=cv2.WARP_FILL_OUTLIERS + cv2.WARP_INVERSE_MAP)
+    polar_image = polar_image.astype(np.uint8)
+    
+    return polar_image
+
+
+cnt = 0
+for _, _, road_img, filename, concat_width_img in trainloader:
+
+    polar_image = warpper(concat_width_img)
+
+    input_res = torch.Tensor(polar_image / 255).unsqueeze(0).permute(0,3,1,2) 
+    output = res_model(input_res)
     save_filename = filename[0].split('/')[-2] + '_' + filename[0].split('/')[-1]
     
-    if not os.path.exists('/Users/leo/Downloads/result/image_tensor'):
-        os.mkdir('/Users/leo/Downloads/result/image_tensor')
-    if not os.path.exists('/Users/leo/Downloads/result/road_map'):
-        os.mkdir('/Users/leo/Downloads/result/road_map')
+    if not os.path.exists('/Users/leo/Downloads/polar/image_tensor'):
+        os.mkdir('/Users/leo/Downloads/polar/image_tensor')
+    if not os.path.exists('/Users/leo/Downloads/polar/road_map'):
+        os.mkdir('/Users/leo/Downloads/polar/road_map')
+    if not os.path.exists('/Users/leo/Downloads/polar/polar_image'):
+        os.mkdir('/Users/leo/Downloads/polar/polar_image')
 
     assert road_img[0].shape == torch.Size([800, 800])
-    assert output.shape == torch.Size([6, 256, 16, 20])
+    assert output.shape == torch.Size([1, 256, 188, 188])
 
-    np.save(os.path.join('/Users/leo/Downloads/result/road_map', save_filename), road_img[0].detach().numpy())    
-    np.save(os.path.join('/Users/leo/Downloads/result/image_tensor', save_filename), output.detach().numpy())
+    np.save(os.path.join('/Users/leo/Downloads/polar/image_tensor', save_filename), output.detach().numpy())
+    np.save(os.path.join('/Users/leo/Downloads/polar/road_map', save_filename), road_img[0].detach().numpy())  
+    cv2_filename = save_filename + '.png'
+    cv2.imwrite(os.path.join('/Users/leo/Downloads/polar/polar_image', cv2_filename), polar_image[...,::-1])
     
-    cnt += 1
-    if cnt % 100 == 0:
+    
+    if cnt % 10 == 0:
         print(str(28 * 126 - cnt) + ' left')
-
-    
+    cnt += 1
