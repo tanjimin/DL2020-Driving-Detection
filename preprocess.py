6e4db25f290d65d510a088dc19608702b5011aa1
Chenqin Yang <cy1355@nyu.edu>
2020-04-25 16:19:43 -0400

    preprocess camera tensors

---
+++ preprocess.py
@@ -0,0 +1,144 @@
+import os
+import random
+from PIL import Image
+import numpy as np
+import pandas as pd
+import cv2
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+import matplotlib.pyplot as plt
+
+image_folder = './data'
+annotation_csv = './data/annotation.csv'
+
+unlabeled_scene_index = np.arange(106)
+labeled_scene_index = np.arange(106, 134)
+
+NUM_SAMPLE_PER_SCENE = 126
+NUM_IMAGE_PER_SAMPLE = 6
+# re-define the camera order
+image_names = [
+    'CAM_FRONT_LEFT.jpeg',
+    'CAM_FRONT.jpeg',
+    'CAM_FRONT_RIGHT.jpeg',
+    'CAM_BACK_RIGHT.jpeg',
+    'CAM_BACK.jpeg',
+    'CAM_BACK_LEFT.jpeg'
+    ]
+
+def convert_map_to_lane_map(ego_map, binary_lane):
+    mask = (ego_map[0,:,:] == ego_map[1,:,:]) * (ego_map[1,:,:] == ego_map[2,:,:]) + (ego_map[0,:,:] == 250 / 255)
+
+    if binary_lane:
+        return (~ mask)
+    return ego_map * (~ mask.view(1, ego_map.shape[1], ego_map.shape[2]))
+
+def convert_map_to_road_map(ego_map):
+    mask = (ego_map[0,:,:] == 1) * (ego_map[1,:,:] == 1) * (ego_map[2,:,:] == 1)
+
+    return (~mask)
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+class LabeledDataset(torch.utils.data.Dataset):    
+    def __init__(self, image_folder, annotation_file, scene_index, transform, extra_info=True):
+        """
+        Args:
+            image_folder (string): the location of the image folder
+            annotation_file (string): the location of the annotations
+            scene_index (list): a list of scene indices for the unlabeled data 
+            transform (Transform): The function to process the image
+            extra_info (Boolean): whether you want the extra information
+        """
+        
+        self.image_folder = image_folder
+        self.annotation_dataframe = pd.read_csv(annotation_file)
+        self.scene_index = scene_index
+        self.transform = transform
+        self.extra_info = extra_info
+    
+    def __len__(self):
+        return self.scene_index.size * NUM_SAMPLE_PER_SCENE
+
+    def __getitem__(self, index):
+        scene_id = self.scene_index[index // NUM_SAMPLE_PER_SCENE]
+        sample_id = index % NUM_SAMPLE_PER_SCENE
+        sample_path = os.path.join(self.image_folder, f'scene_{scene_id}', f'sample_{sample_id}') 
+
+        images = []
+        for image_name in image_names:
+            image_path = os.path.join(sample_path, image_name)
+            image = Image.open(image_path)
+            images.append(self.transform(image))
+        image_tensor = torch.stack(images)
+        
+
+        data_entries = self.annotation_dataframe[(self.annotation_dataframe['scene'] == scene_id) & (self.annotation_dataframe['sample'] == sample_id)]
+        corners = data_entries[['fl_x', 'fr_x', 'bl_x', 'br_x', 'fl_y', 'fr_y','bl_y', 'br_y']].to_numpy()
+        categories = data_entries.category_id.to_numpy()
+        
+        ego_path = os.path.join(sample_path, 'ego.png')
+        ego_image = Image.open(ego_path)
+        ego_image = torchvision.transforms.functional.to_tensor(ego_image)
+        road_image = convert_map_to_road_map(ego_image)
+        
+        target = {}
+        target['bounding_box'] = torch.as_tensor(corners).view(-1, 2, 4)
+        target['category'] = torch.as_tensor(categories)
+
+        if self.extra_info:
+            actions = data_entries.action_id.to_numpy()
+            # You can change the binary_lane to False to get a lane with 
+            lane_image = convert_map_to_lane_map(ego_image, binary_lane=True)
+            
+            extra = {}
+            extra['action'] = torch.as_tensor(actions)
+            extra['ego_image'] = ego_image
+            extra['lane_image'] = lane_image
+
+            return image_tensor, target, road_image, extra
+        else:
+            return image_tensor, target, road_image, sample_path
+
+transform = torchvision.transforms.ToTensor()
+labeled_trainset = LabeledDataset(image_folder=image_folder,
+                                  annotation_file=annotation_csv,
+                                  scene_index=labeled_scene_index,
+                                  transform=transform,
+                                  extra_info=False
+                                 )
+trainloader = torch.utils.data.DataLoader(labeled_trainset, batch_size=1, shuffle=True, num_workers=0, collate_fn=collate_fn)
+
+# load model
+pretrained_model = torchvision.models.resnet18(pretrained=True)
+modules = list(pretrained_model.children())[:-3]
+res_model = nn.Sequential(*modules)
+res_model.eval()
+
+cnt = 0
+for data, _, road_img, filename in trainloader:
+
+    output = res_model(data[0])
+    save_filename = filename[0].split('/')[-2] + '_' + filename[0].split('/')[-1]
+    
+    if not os.path.exists('/Users/leo/Downloads/result/image_tensor'):
+        os.mkdir('/Users/leo/Downloads/result/image_tensor')
+    if not os.path.exists('/Users/leo/Downloads/result/road_map'):
+        os.mkdir('/Users/leo/Downloads/result/road_map')
+
+    assert road_img[0].shape == torch.Size([800, 800])
+    assert output.shape == torch.Size([6, 256, 16, 20])
+
+    np.save(os.path.join('/Users/leo/Downloads/result/road_map', save_filename), road_img[0].detach().numpy())    
+    np.save(os.path.join('/Users/leo/Downloads/result/image_tensor', save_filename), output.detach().numpy())
+    
+    cnt += 1
+    if cnt % 100 == 0:
+        print(str(28 * 126 - cnt) + ' left')
+
+    
