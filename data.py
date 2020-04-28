d9458502a54b09c6ee3653eb18e510938a97d9a1
Chenqin Yang <cy1355@nyu.edu>
2020-04-27 22:58:18 -0400

    add Front dataloader

--- data.py
+++ data.py
@@ -22,3 +22,26 @@ class LaneSegmentationDataset(Dataset):
         label = (np.load(label_path) * 1).astype(np.single)
         return data, label
         
+
+
+class Front_LaneSegmentationDataset(Dataset):
+    
+    def __init__(self, data_dir, label_dir):
+        self.data_dir = data_dir
+        self.label_dir = label_dir
+        self.data_names = sorted(os.listdir(data_dir))
+
+    def __len__(self):
+        return len(self.data_names)
+
+    def __getitem__(self, idx):
+        data_path = os.path.join(self.data_dir, self.data_names[idx])
+        label_path = os.path.join(self.label_dir, self.data_names[idx])
+        print(label_path)
+        data = np.load(data_path)
+        label = (np.load(label_path) * 1).astype(np.single)
+        
+        # data: CAM_FRONT_LEFT, CAM_FRONT, CAM_FRONT_RIGHT, CAM_BACK_LEFT, CAM_BACK, CAM_BACK_RIGHT
+        # label: [height = 538, width = 400] ---> rotate counterclockwise [h = 400, width = 538]
+        label = torch.from_numpy(np.rot90(label[131:669,400:]).copy())
+        return data[1,:], label
