ff5076e9f11738b2cc00decf28caac47a274a0a9
Chenqin Yang <cy1355@nyu.edu>
2020-04-28 16:39:40 -0400

    update bbox dataloader

--- data.py
+++ data.py
@@ -3,7 +3,10 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import numpy as np
+import pandas as pd
 from torch.utils.data import Dataset
+
+
 
 class LaneSegmentationDataset(Dataset):
     
@@ -44,3 +47,36 @@ class FrontLaneSegmentationDataset(Dataset):
         # label: [height = 538, width = 400] ---> rotate counterclockwise [h = 400, width = 538]
         label = torch.from_numpy(np.rot90(label[131:669,400:]).copy())
         return data[1,:], label
+
+class FrontObjectSegmentationDataset(Dataset):
+    
+    def __init__(self, data_dir, label_dir, annotation_file):
+        self.data_dir = data_dir
+        self.label_dir = label_dir
+        self.data_names = sorted(os.listdir(data_dir))
+        self.annotation_dataframe = pd.read_csv(annotation_file)
+
+    def __len__(self):
+        return len(self.data_names)
+
+    def __getitem__(self, idx):
+        data_path = os.path.join(self.data_dir, self.data_names[idx])
+        data = np.load(data_path)
+        
+        # # data: CAM_FRONT_LEFT, CAM_FRONT, CAM_FRONT_RIGHT, CAM_BACK_LEFT, CAM_BACK, CAM_BACK_RIGHT
+        # # label: [height = 538, width = 400] ---> rotate counterclockwise [h = 400, width = 538]
+        # label = torch.from_numpy(np.rot90(label[131:669,400:]).copy())
+        scene_id = int(self.data_names[idx].strip('.npy').split('_')[1])
+        sample_id = int(self.data_names[idx].strip('.npy').split('_')[-1])
+
+        data_entries = self.annotation_dataframe[(self.annotation_dataframe['scene'] == scene_id) & (self.annotation_dataframe['sample'] == sample_id)]
+        corners = data_entries[['fl_x', 'fr_x', 'bl_x', 'br_x', 'fl_y', 'fr_y','bl_y', 'br_y']].to_numpy()
+        categories = data_entries.category_id.to_numpy()
+        
+        target = {}
+        target['bounding_box'] = torch.as_tensor(corners).view(-1, 2, 4)
+        target['category'] = torch.as_tensor(categories)
+
+        # data: (256, 16, 20)
+        # target: len = 2, dict with 2 pairs of key-value
+        return data[1,:], target
