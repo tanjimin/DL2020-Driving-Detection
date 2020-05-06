3e51459303f5df20212597ad5e71d4e695ebb519
Chenqin Yang <cy1355@nyu.edu>
2020-05-06 18:20:35 -0400

    modify camera concat method

--- utils.py
+++ utils.py
@@ -69,13 +69,13 @@ def concat_cameras(outputs):
 
     rot_axes = (2,1)
 
-    init_out = np.zeros(batch_n, 800,800)
+    init_out = np.zeros((batch_n, 800,800))
     # rotate counter-clockwise
     # assign front left and back right cameras
     # then rotate clockwise back and crop out 800*800
     init_out = ndimage.rotate(init_out, 30, axes = rot_axes)
-    init_out[:, 547 - 400:547, 278: 278 + 538] = front_left.copy()
-    init_out[:, 547:547+400, 278: 278 + 538][::-1,::-1] = back_right.copy()
+    init_out[:, 547 - 400:547, 278: 278 + 538] += front_left.copy()
+    init_out[:, 547:547+400, 278: 278 + 538] += back_right[:, ::-1,::-1].copy()
     init_out = ndimage.rotate(init_out, -30, axes = rot_axes)
     init_out = init_out[:, 347:347+800, 347:347+800]
 
@@ -83,14 +83,14 @@ def concat_cameras(outputs):
     # assign front right and back right cameras
     # then rotate counter-clockwise back and crop out 800*800
     init_out = ndimage.rotate(init_out, -30, axes = rot_axes)
-    init_out[:, 547-400:547, 278: 278 + 538 ] = back_left.copy()
-    init_out[:, 547:547+400, 278: 278 + 538 ][::-1,::-1] = front_right.copy()
+    init_out[:, 547-400:547, 278: 278 + 538 ] += back_left.copy()
+    init_out[:, 547:547+400, 278: 278 + 538 ] += front_right[:, ::-1,::-1].copy()
     init_out = ndimage.rotate(init_out, 30, axes = rot_axes)
     init_out = init_out[:, 347:347+800, 347:347+800]
 
     # inverse the process for front/back as well
-    init_out[:, 131:131+538, 400:] = ndimage.rotate(front, -90, axes = rot_axes)
-    init_out[:, 131:131+538, :400][::-1,::-1] = ndimage.rotate(back, -90, axes = rot_axes)
+    init_out[:, 131:131+538, 400:] += ndimage.rotate(front, -90, axes = rot_axes)
+    init_out[:, 131:131+538, :400] += ndimage.rotate(back[:, ::-1,::-1], -90, axes = rot_axes)
 
     init_out = torch.tensor(init_out)
 
